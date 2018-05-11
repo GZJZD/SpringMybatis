641729bed89df4920bccfb6365319323d6523cc9
a38519040@163.com <23905344>
2018-05-11 15:22:41 +0800

    update TestControlller

--- src/main/java/com/web/controller/TestController.java
+++ src/main/java/com/web/controller/TestController.java
@@ -2,6 +2,7 @@ package com.web.controller;
 
 import com.web.pojo.Test;
 import com.web.service.TestService;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
@@ -9,6 +10,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import static org.apache.log4j.Logger.getRootLogger;
 
 
 /**
@@ -21,9 +24,13 @@ public class TestController {
     @Qualifier("testService")
     private TestService testService;
 
+
+
+
     @RequestMapping(value = "/get.Action", method = RequestMethod.GET)
     @ResponseBody
     public Test getTestById(@RequestParam("id") String id) {
+
         System.out.println("hello come in");
         return testService.getById(Integer.parseInt(id)
 
