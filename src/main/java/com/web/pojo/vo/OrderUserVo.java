4b1073798255d04a1287379c9f7ece9565aaf19f
a38519040@163.com <23905344>
2018-05-21 15:41:59 +0800

    add OrderuserVo

---
+++ src/main/java/com/web/pojo/vo/OrderUserVo.java
@@ -0,0 +1,94 @@
+package com.web.pojo.vo;
+
+
+/**
+ * 用户列表映射（此类没表，只是映射）
+ */
+public class OrderUserVo {
+    /**
+     * 用户Code
+     */
+    private Integer userCode;
+    /**
+     * 用户名称
+     */
+     private String userName;
+    /**
+     * 平台名称
+     */
+     private String platformName;
+    /**
+     * 代理人
+     */
+    private String agencyName;
+    /**
+     * 最近下单时间
+     */
+    private String latestOrderTime;
+    /**
+     * 胜率
+     */
+    private Double winRate;
+    /**
+     * 入金
+     */
+    private Double inMoney;
+    /**
+     * 出金
+     */
+    private Double outMoney;
+    /**
+     * 用户类型
+     */
+    private String userType;
+    /**
+     * 手续费总额
+     */
+    private Double serviceChargeTotal;
+
+
+
+
+
+    /**
+     * 持仓盈亏
+     */
+    private Double position_gain_and_loss;
+    /**
+     * 平仓盈亏
+     */
+    private Double offset_gain_and_loss;
+    /**
+     * 累计盈亏
+     */
+
+    /**
+     * 回报率
+     */
+
+    /**
+     *盈亏比
+     */
+    private Double profit_loss_than;
+
+    /**
+     * 做单数
+     */
+    private Double do_singular;
+
+
+
+
+
+
+
+    /**
+     * 做单天数
+     */
+    private Integer documentary_total_day;
+
+
+
+
+
+}
