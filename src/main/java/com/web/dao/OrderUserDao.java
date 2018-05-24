5fae96ab9bee8090553eac765fd0c01e8f4521a5
a38519040@163.com <23905344>
2018-05-24 17:14:05 +0800

    dev

--- src/main/java/com/web/dao/OrderUserDao.java
+++ src/main/java/com/web/dao/OrderUserDao.java
@@ -1,9 +1,11 @@
 package com.web.dao;
 
+import com.web.pojo.DataSource;
 import com.web.pojo.OrderUser;
 
 import java.util.List;
 
 public interface OrderUserDao {
     List<OrderUser> findAll();
+    void addOrderUser(OrderUser orderUser);
 }
--- src/main/java/com/web/pojo/OrderUser.java
+++ src/main/java/com/web/pojo/OrderUser.java
@@ -6,35 +6,38 @@ import com.web.util.common.BaseUtil;
  * 客户列表
  */
 public class OrderUser  extends BaseUtil {
-
     /**
      * 用户编号
      */
-    private Long userCode;
+    private String userCode;
 
     /**
-     * 平台
+     *开仓单号
      */
-    private Long platformCode;
+     private String ticket;
 
     /**
-     * 代理人
+     * 商品
      */
-    private String agencyName;
+    private String produceCode;
 
     /**
-     * 持仓数
+     * 多空
      */
-    private Long holdNumber;
-    /**
-     * 平仓盈亏
-     */
-    private Double closeGainLoss;
-    /**
-     * 品种
-     */
-    private Long produceCode;
+    private Integer longShort;
 
+    /**
+     * 手数
+     */
+    private Double handNumber;
+    /**
+     * 开仓价
+     */
+     private  Double openPrice;
+    /**
+     * 平仓价位
+     */
+    private Double closePrice;
     /**
      * 开仓时间
      */
@@ -44,6 +47,45 @@ public class OrderUser  extends BaseUtil {
      * 平仓时间
      */
     private String  closeTime;
+    /**
+     * 平仓盈亏
+     */
+    private Double profit;
+    /**
+     * 价位
+     */
+    private Double price;
+
+    /**
+     * 平台
+     */
+    private String platformCode;
+
+    /**
+     * 代理人
+     */
+    private String agencyName;
+
+
+    /**************后期可能新追加字段*******************/
+
+    /**
+     * 买入价
+     */
+    private Double bid;
+    /**
+     * 卖出价
+     */
+    private Double ask;
+
+    /**
+     * 每日高价
+     */
+    private Double high;
+    /**
+     * 每日最低价
+     */
+    private Double low;
 
     /**
      * 入金
@@ -55,14 +97,159 @@ public class OrderUser  extends BaseUtil {
     private Double outMoney;
 
 
-    /**
-     * 手数
-     */
-    private Double handNumber;
 
-    /**
-     * 价位
-     */
-    private Double price;
 
+
+
+    public String getUserCode() {
+        return userCode;
+    }
+
+    public void setUserCode(String userCode) {
+        this.userCode = userCode;
+    }
+
+    public String getTicket() {
+        return ticket;
+    }
+
+    public void setTicket(String ticket) {
+        this.ticket = ticket;
+    }
+
+    public String getProduceCode() {
+        return produceCode;
+    }
+
+    public void setProduceCode(String produceCode) {
+        this.produceCode = produceCode;
+    }
+
+    public Integer getLongShort() {
+        return longShort;
+    }
+
+    public void setLongShort(Integer longShort) {
+        this.longShort = longShort;
+    }
+
+    public Double getHandNumber() {
+        return handNumber;
+    }
+
+    public void setHandNumber(Double handNumber) {
+        this.handNumber = handNumber;
+    }
+
+    public Double getOpenPrice() {
+        return openPrice;
+    }
+
+    public void setOpenPrice(Double openPrice) {
+        this.openPrice = openPrice;
+    }
+
+    public Double getClosePrice() {
+        return closePrice;
+    }
+
+    public void setClosePrice(Double closePrice) {
+        this.closePrice = closePrice;
+    }
+
+    public String getOpenTime() {
+        return openTime;
+    }
+
+    public void setOpenTime(String openTime) {
+        this.openTime = openTime;
+    }
+
+    public String getCloseTime() {
+        return closeTime;
+    }
+
+    public void setCloseTime(String closeTime) {
+        this.closeTime = closeTime;
+    }
+
+    public Double getProfit() {
+        return profit;
+    }
+
+    public void setProfit(Double profit) {
+        this.profit = profit;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getPlatformCode() {
+        return platformCode;
+    }
+
+    public void setPlatformCode(String platformCode) {
+        this.platformCode = platformCode;
+    }
+
+    public String getAgencyName() {
+        return agencyName;
+    }
+
+    public void setAgencyName(String agencyName) {
+        this.agencyName = agencyName;
+    }
+
+    public Double getBid() {
+        return bid;
+    }
+
+    public void setBid(Double bid) {
+        this.bid = bid;
+    }
+
+    public Double getAsk() {
+        return ask;
+    }
+
+    public void setAsk(Double ask) {
+        this.ask = ask;
+    }
+
+    public Double getHigh() {
+        return high;
+    }
+
+    public void setHigh(Double high) {
+        this.high = high;
+    }
+
+    public Double getLow() {
+        return low;
+    }
+
+    public void setLow(Double low) {
+        this.low = low;
+    }
+
+    public Double getInMoney() {
+        return inMoney;
+    }
+
+    public void setInMoney(Double inMoney) {
+        this.inMoney = inMoney;
+    }
+
+    public Double getOutMoney() {
+        return outMoney;
+    }
+
+    public void setOutMoney(Double outMoney) {
+        this.outMoney = outMoney;
+    }
 }
