d427b107e22699e4687f05416a9c89f35a827f3f
a38519040@163.com <23905344>
2018-06-12 09:54:48 +0800

    dev

--- src/main/java/com/web/service/imp/ProducerMsgServiceImpl.java
+++ src/main/java/com/web/service/imp/ProducerMsgServiceImpl.java
@@ -4,6 +4,7 @@ import com.web.service.IProducerMsgService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.jms.core.JmsTemplate;
+
 import org.springframework.jms.core.MessageCreator;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
