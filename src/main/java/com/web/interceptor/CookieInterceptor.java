a221537f11886da2f09413c0293fc5cfc5db5185
a38519040@163.com <23905344>
2018-06-29 16:39:55 +0800

    login 代碼優化

--- src/main/java/com/web/interceptor/CookieInterceptor.java
+++ src/main/java/com/web/interceptor/CookieInterceptor.java
@@ -1,4 +1,6 @@
 package com.web.interceptor;
+import java.io.UnsupportedEncodingException;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
@@ -34,69 +36,78 @@ public class CookieInterceptor extends HandlerInterceptorAdapter {
      */
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
             throws Exception {
-        System.out.println("=================test=================="+request.getContextPath());
                 HttpSession session = request.getSession();
                 Object o  =   session.getAttribute("employee");
-                long outTime = CookieConstantTable.outTime;
-                System.out.println("*****************************************:"+o);
-                Cookie rememberme = CookieUtils.getCookie(request, "token");
-
-        // 已登录
-        if (o != null) {
-
-                    String str =EncryptionUtil.base64Decode(String.valueOf(o));
-                    String[] strList = str.split("_");
-                    String phoneNumber = strList[0];
-                    System.out.println(phoneNumber);
-                    String verifyTime = strList[1];
-                    Login login = loginService.findByPhoneNumber(phoneNumber);
-                    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-                    Date begin_Time = sdf.parse(login.getVerifyTime());
-                    Date end_Time = sdf.parse(DateUtil.getStringDate());
-                    long days = (end_Time.getTime()-begin_Time.getTime());//当前时间 减去最后验证时间
-                    if(days > outTime){
-                        //有效期已经超时，需要重新登录 重定向login 页面
-                        CookieUtils.delCookie(response,rememberme);
+                 Cookie cookie = CookieUtils.getCookie(request, "token");
+             // 已登录
+            if (cookie != null) {
+                chenckTime(cookie.getValue(),response,cookie);
+                        return true;
+            } else {
+                if( cookie != null){
+                        //判断是否超时
+                        chenckTime(cookie.getValue(),response,cookie);
+                        return true;
+                    }else {
+                        session.removeAttribute("employee");
+                        session.removeAttribute("JSESSIONID");
+                        session.invalidate();
+                        CookieUtils.delCookie(response,cookie);
+                        String toLoginPath=request.getContextPath() + "/Login/goto.Action?gotoPage="+"gotoPage";
+                        //如果request.getHeader("X-Requested-With") 返回的是"XMLHttpRequest"说明就是ajax请求，需要特殊处理
+                        if("XMLHttpRequest".equals(request.getHeader("X-Requested-With"))){
+                            response.setHeader("REDIRECT", "REDIRECT");//标识
+                            response.setHeader("CONTENTPATH",toLoginPath);//跳转地址
+                            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+                        }else{
+                            //重定向跳轉
+                            TopPageOpenUtils.sendRedirect(toLoginPath,response);
+                        }
+                        return false;
                     }
-
-                return true;
-        } else {
-
-            Cookie cookie = CookieUtils.getCookie(request, "token");
-            if( cookie != null){
-                //判断是否超时
-                System.out.println("not null");
-                System.out.println(cookie.getName());
-                System.out.println(cookie.getValue());
-                return true;
-            }else {
-                System.out.println("is null");
-                session.removeAttribute("employee");
-                session.removeAttribute("JSESSIONID");
-                session.invalidate();
-                CookieUtils.delCookie(response,rememberme);
-                String toLoginPath=request.getContextPath() + "/Login/goto.Action?gotoPage="+"gotoPage";
-
-                //response.sendRedirect("https://www.baidu.com/");
-
-                //如果request.getHeader("X-Requested-With") 返回的是"XMLHttpRequest"说明就是ajax请求，需要特殊处理
-                if("XMLHttpRequest".equals(request.getHeader("X-Requested-With"))){
-                    response.setHeader("REDIRECT", "REDIRECT");//标识
-                    response.setHeader("CONTENTPATH",toLoginPath);//跳转地址
-                    response.setStatus(HttpServletResponse.SC_FORBIDDEN);
-                }else{
-                    //重定向跳轉
-                    TopPageOpenUtils.sendRedirect(toLoginPath,response);
-                }
-                return false;
             }
-        }
 
-    }
+     }
 
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
             throws Exception {
         super.afterCompletion(request, response, handler, ex);
     }
 
+    public void chenckTime(String value,HttpServletResponse response,Cookie cookie){
+        boolean checkStatus = false;
+        long outTime = CookieConstantTable.outTime;
+        String[] strList = new String[0];
+        try {
+            strList = EncryptionUtil.base64Decode(value).split("_");
+            String phoneNumber = strList[0];
+            String verifyTime = strList[1];
+            Login login = loginService.findByPhoneNumber(phoneNumber);
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+            Date begin_Time = null;
+            try {
+                begin_Time = sdf.parse(login.getVerifyTime());
+                Date end_Time = sdf.parse(DateUtil.getStringDate());
+                long days = (end_Time.getTime()-begin_Time.getTime());//当前时间 减去最后验证时间
+                if(days < outTime){
+                   checkStatus = true;
+                }else {
+                    checkStatus = false;
+                }
+            } catch (ParseException e) {
+
+                log.error("登录日期转换出错！");
+                e.printStackTrace();
+            }
+
+
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+            log.error("登录日期转换出错！");
+        }
+        if (checkStatus == true){
+            CookieUtils.delCookie(response,cookie);
+        }
+
+    }
 }
