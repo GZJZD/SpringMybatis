1276ac23d43d738e5d29b739f5bac9e0351e13fd
May <jumy_z@126.com>
2018-07-02 17:02:00 +0800

    dev

--- src/main/java/com/web/util/json/WebJsion.java
+++ src/main/java/com/web/util/json/WebJsion.java
@@ -102,39 +102,4 @@ public class WebJsion {
     }
 
 
-    /**
-     *
-     * @param source 被复制的实体类对象
-     * @param to 复制完后的实体类对象
-     * @throws Exception
-     */
-    public static void Copy(Object source, Object to) throws Exception {
-        // 获取属性
-        BeanInfo sourceBean = Introspector.getBeanInfo(source.getClass(),java.lang.Object.class);
-        PropertyDescriptor[] sourceProperty = sourceBean.getPropertyDescriptors();
-
-        BeanInfo destBean = Introspector.getBeanInfo(to.getClass(),java.lang.Object.class);
-        PropertyDescriptor[] destProperty = destBean.getPropertyDescriptors();
-
-        try {
-            for (int i = 0; i < sourceProperty.length; i++) {
-
-                for (int j = 0; j < destProperty.length; j++) {
-
-                    if (sourceProperty[i].getName().equals(destProperty[j].getName())) {
-                        // 调用source的getter方法和dest的setter方法
-                        destProperty[j].getWriteMethod().invoke(to,sourceProperty[i].getReadMethod().invoke(source));
-                        break;
-                    }
-                }
-            }
-        } catch (Exception e) {
-            throw new Exception("属性复制失败:" + e.getMessage());
-        }
-    }
-
-
-
-
-
 }
