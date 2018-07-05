565833de21d876a64a3c0feb28f1ef8a67865a82
Administrator <Administrator@WG0YX1RBIXJBB08>
2018-07-05 16:38:08 +0800

    品种信息更改

--- src/main/java/com/web/controller/VarietyController.java
+++ src/main/java/com/web/controller/VarietyController.java
@@ -1,12 +1,12 @@
 package com.web.controller;
 
-import com.alibaba.fastjson.JSON;
-import com.web.service.IVarietyService;
-import com.web.util.json.WebJsion;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.web.service.IVarietyService;
+import com.web.util.json.WebJsion;
 
 @Controller
 @RequestMapping("/variety")
