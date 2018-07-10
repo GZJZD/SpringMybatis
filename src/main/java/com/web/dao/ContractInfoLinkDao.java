da17a499d69fe033601f335b197d4ac03b049979
Administrator <Administrator@WG0YX1RBIXJBB08>
2018-07-10 15:03:41 +0800

    合约链接信息更改

---
+++ src/main/java/com/web/dao/ContractInfoLinkDao.java
@@ -0,0 +1,12 @@
+package com.web.dao;
+
+import com.web.pojo.ContractInfo;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+/**
+ * Created by may on 2018/5/21.
+ */
+public interface ContractInfoLinkDao {
+}
