b111247fe9d111d82c79c56e8bf5ce3f8bb9e72d
a38519040@163.com <23905344>
2018-07-10 20:28:34 +0800

    update

--- src/main/java/com/web/util/common/DoubleUtil.java
+++ src/main/java/com/web/util/common/DoubleUtil.java
@@ -1,11 +1,13 @@
 package com.web.util.common;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 /**
  * Created by may on 2018/5/9.
  */
 public abstract class DoubleUtil {
+    public static final Double Double_val = 0.00;
     private static final int DEF_DIV_SCALE = 10;
 
     /**
@@ -48,6 +50,15 @@ public abstract class DoubleUtil {
     }
 
     /**
+     * double 和 int 相乘计算
+     */
+    public static Double mul(Double v1, Integer v2){
+        BigDecimal b1 = new BigDecimal(Integer.toString(v2));
+        BigDecimal b2 = new BigDecimal(Double.toString(v1));
+        return new Double( b1.multiply(b2).doubleValue());
+    }
+
+    /**
      * * 两个Double数相除 *
      *
      * @param v1 *
@@ -60,6 +71,7 @@ public abstract class DoubleUtil {
         return new Double(b1.divide(b2, DEF_DIV_SCALE, BigDecimal.ROUND_HALF_UP)
                 .doubleValue());
     }
+
 
     /**
      * * 两个Double数相除，并保留scale位小数 *
@@ -78,4 +90,21 @@ public abstract class DoubleUtil {
         BigDecimal b2 = new BigDecimal(v2.toString());
         return new Double(b1.divide(b2, scale, BigDecimal.ROUND_HALF_UP).doubleValue());
     }
+
+    /**
+     * Double 类型除以 正数类型
+     *     @param v1 *
+     *      @param v2 *
+     *      scale  保留多少位数
+     *  @return Double
+     */
+     public static Double div(Double v1 ,Integer v2, int scale){
+         if (scale < 0) {
+             throw new IllegalArgumentException(
+                     "The scale must be a positive integer or zero");
+         }
+         BigDecimal b2 = new BigDecimal(Integer.toString(v2));
+         BigDecimal b1 = new BigDecimal(Double.toString(v1));
+         return b1.divide(b2,scale).doubleValue();
+     }
 }
