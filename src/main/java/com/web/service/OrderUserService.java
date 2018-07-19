15b7fc4372b9605883e16480161de8b3f33f4837
a38519040@163.com <23905344>
2018-07-19 15:01:25 +0800

    dev

--- src/main/java/com/web/service/OrderUserService.java
+++ src/main/java/com/web/service/OrderUserService.java
@@ -7,6 +7,7 @@ import com.web.pojo.vo.orderuser.OrderUserListVo;
 import com.web.pojo.vo.orderuser.OrderUserVo;
 
 import java.util.List;
+import java.util.Map;
 
 public interface OrderUserService {
     List<OrderUser> findAll();
@@ -14,7 +15,7 @@ public interface OrderUserService {
     OrderUser findByTicket(String ticket);
     String update(OrderUser orderUser);
     OrderUserListVo countOrderUser(OrderUserVo orderUserVo);
-    List<OrderUser>findByUserIdList(List<String> list, String startTime, String endTime, String productCode, Integer openOrCloseStatus);
+    List<OrderUser>findByUserIdList(Map<String, String> map, String startTime, String endTime, String productCode, Integer openOrCloseStatus);
     OrderUserDetailsVo getUserDetails(String userCode, String productCode,String platFormCode);
     OrderUserDetailsVo  getOrderUserCount(String userCode, String productCode,String platFormCode);
 
