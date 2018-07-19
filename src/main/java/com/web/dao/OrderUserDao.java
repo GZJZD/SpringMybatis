3f80841f80eff54a209eaffe95a97081da0f8579
a38519040@163.com <23905344>
2018-07-19 15:00:55 +0800

    dev

--- src/main/java/com/web/dao/OrderUserDao.java
+++ src/main/java/com/web/dao/OrderUserDao.java
@@ -6,7 +6,7 @@ import com.web.pojo.vo.orderuser.OrderUserVo;
 import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
-
+import java.util.Map;
 
 
 public interface OrderUserDao {
@@ -15,7 +15,7 @@ public interface OrderUserDao {
     OrderUser findByTicket(String  ticket);
     void update(OrderUser orderUser);
     List<OrderUser> countOrderUser(OrderUserVo orderUserVo);
-    List<OrderUser> findByUserIdList(@Param("list") List<String> list , @Param("startTime")  String startTime , @Param("endTime") String endTime , @Param("productCode") String productCode, @Param("status") Integer status);
+    List<OrderUser> findByUserIdList(@Param("list") Map<String, String> map , @Param("startTime")  String startTime , @Param("endTime") String endTime , @Param("productCode") String productCode, @Param("status") Integer status);
     List<OrderUser> getUserDetails(@Param("userCode")  String userCode,@Param("productCode")  String productCode , @Param("platFormCode")  String platFormCode);
 
 
